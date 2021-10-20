7e3ceb95bd6132707e9e99df513fa1cb5bcec012
Agata Figura <agata.figura1@gmail.com>
2021-10-20 12:15:38 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const init = function(){
+    cssScrollSnapPolyfill()
+  }
+
+init();
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
